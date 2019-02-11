5850adf3803f170778150e8be72c8301c28445f6
lyngbo1109 <31726129+lyngbo1109@users.noreply.github.com>
2019-02-11 22:33:10 +0100

    Fixing errors

--- Tarnished-Mainframe/Tarnished-Mainframe/TarnishedMainframe.cpp
+++ Tarnished-Mainframe/Tarnished-Mainframe/TarnishedMainframe.cpp
@@ -1,2 +1,17 @@
 #include "TarnishedMainframe.h"
 
+
+using namespace System;
+using namespace System::Windows::Forms;
+#include <string>
+
+[STAThread]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TarnishedMainframe::TarnishedMainframe start_screen;
+	Application::Run(%start_screen);
+
+	return 0;
+}
--- Tarnished-Mainframe/Tarnished-Mainframe/TarnishedMainframe.h
+++ Tarnished-Mainframe/Tarnished-Mainframe/TarnishedMainframe.h
@@ -34,6 +34,11 @@ namespace TarnishedMainframe {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::RadioButton^  radioButton2;
+	private: System::Windows::Forms::RadioButton^  radioButton1;
+	private: System::Windows::Forms::GroupBox^  groupBox2;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +53,70 @@ namespace TarnishedMainframe {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"TarnishedMainframe";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->radioButton2);
+			this->groupBox1->Controls->Add(this->radioButton1);
+			this->groupBox1->Location = System::Drawing::Point(12, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(1071, 218);
+			this->groupBox1->TabIndex = 0;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Setup";
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Location = System::Drawing::Point(12, 236);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(1071, 262);
+			this->groupBox2->TabIndex = 1;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"groupBox2";
+			// 
+			// radioButton1
+			// 
+			this->radioButton1->AutoSize = true;
+			this->radioButton1->Location = System::Drawing::Point(7, 20);
+			this->radioButton1->Name = L"radioButton1";
+			this->radioButton1->Size = System::Drawing::Size(85, 17);
+			this->radioButton1->TabIndex = 0;
+			this->radioButton1->TabStop = true;
+			this->radioButton1->Text = L"radioButton1";
+			this->radioButton1->UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this->radioButton2->AutoSize = true;
+			this->radioButton2->Location = System::Drawing::Point(7, 44);
+			this->radioButton2->Name = L"radioButton2";
+			this->radioButton2->Size = System::Drawing::Size(85, 17);
+			this->radioButton2->TabIndex = 1;
+			this->radioButton2->TabStop = true;
+			this->radioButton2->Text = L"radioButton2";
+			this->radioButton2->UseVisualStyleBackColor = true;
+			// 
+			// TarnishedMainframe
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1092, 509);
+			this->Controls->Add(this->groupBox2);
+			this->Controls->Add(this->groupBox1);
+			this->Name = L"TarnishedMainframe";
+			this->Text = L"Tarnished Mainframe";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+
 	};
 }
